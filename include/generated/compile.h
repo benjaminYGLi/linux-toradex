22a59d752826fba4db5b85a2e82516b7adab209c
benjamin.li <benjamin.li@cloudminds.com>
2017-12-14 15:16:00 +0800

    Init: add init release from toradex
    Details:
    	Add apalis_pcie_rc and ep_defconfig;

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP Thu Nov 23 17:30:01 CST 2017"
+#define LINUX_COMPILE_BY "benjaminli"
+#define LINUX_COMPILE_HOST "benjaminli-Precision-T1700"
+#define LINUX_COMPILER "gcc version 6.2.1 20161016 (Linaro GCC 6.2-2016.11) "
